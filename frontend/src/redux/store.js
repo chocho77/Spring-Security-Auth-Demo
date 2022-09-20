4e27ddc02aef3e122e504ca4735187b873157632
chocho77 <chavmom@gmail.com>
2022-09-20 12:35:31 +0300

    del

--- frontend/src/redux/store.js
+++
@@ -1,6 +0,0 @@
-// import { legacy_createStore as createStore } from "redux";
-// import rootReducer from "./reducers";
-
-// const store=legacy_createStore(rootReducer);
-
-// export default store;
